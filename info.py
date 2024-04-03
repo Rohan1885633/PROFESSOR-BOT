7b5b93d54c9342caa132c2beb25aedceb593ae6d
Rohan1885633 <141541061+Rohan1885633@users.noreply.github.com>
2024-04-04 00:12:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,20 +63,20 @@ P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', "True"), True)
 PM_IMDB = is_enabled(environ.get('PM_IMDB', "True"), True)
 IMDB = is_enabled(environ.get('IMDB', "True"), True)
 SINGLE_BUTTON = is_enabled(environ.get('SINGLE_BUTTON', "True"), True)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "{file_caption}
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION",  '''{file_caption}
 
 ⚙️ Size » {file_size}
 
 ╔═════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╗
 💥 𝙅𝙊𝙄𝙉 :- [Tʜᴇ Mᴏᴠɪᴇs 🍿](https://t.me/The_Movies_Officially)
-╚═════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╝)
-BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", {file_caption}
+╚═════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╝) '''
+BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION",  '''{file_caption}
 
 ⚙️ Size » {file_size}
 
 ╔═════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╗
 💥 𝙅𝙊𝙄𝙉 :- [Tʜᴇ Mᴏᴠɪᴇs 🍿](https://t.me/The_Movies_Officially)
-╚═════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╝)
+╚═════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╝) '''
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
